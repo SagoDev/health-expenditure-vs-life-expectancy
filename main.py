b723271ca1a91b3fd829976367889209e39002a7
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-02-25 16:35:11 -0300

    update(main.py): add dataframes merge execution

--- main.py
+++ main.py
@@ -2,14 +2,19 @@
 
 from src.config import INDICATORS
 from src.data_loader import extract_and_store
-from src.data_cleaning import clean_all_indicators, save_cleaned_indicators
+from src.data_cleaning import (
+    clean_all_indicators,
+    save_cleaned_indicators,
+    build_final_dataset,
+    save_final_dataset,
+)
 
 
 def main():
     """Main function to orchestrate the data pipeline."""
 
     # 1️⃣ Extract
-    # extract_and_store(INDICATORS)
+    extract_and_store(INDICATORS)
 
     # 2️⃣ Clean
     cleaned_data = clean_all_indicators(INDICATORS)
@@ -17,9 +22,14 @@ def main():
     # 3️⃣ Save cleaned indicators
     save_cleaned_indicators(cleaned_data)
 
-    # 4️⃣ Feature engineering
-    # 5️⃣ Analysis
-    # 6️⃣ Save final dataset
+    # 4️⃣ Build final dataset
+    final_df = build_final_dataset(cleaned_data)
+
+    # 5️⃣ Save final dataset
+    save_final_dataset(final_df)
+
+    # 6️⃣ Feature engineering (next step)
+    # 7️⃣ Analysis
 
 
 if __name__ == "__main__":
